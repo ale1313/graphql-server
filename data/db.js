b034d0aa48cb9c50cfd3495a9c8426efdfa9aa2b
ale1313 <alejo.vs99@gmail.com>
2020-01-02 21:34:13 -0300

    Implemented mongoose

---
+++ data/db.js
@@ -0,0 +1,20 @@
+// LIBS
+import mongoose from 'mongoose';
+
+// ────────────────────────────────────────────────────────────────────────────────────────────────
+
+mongoose.Promise = global.Promise;
+
+mongoose.connect('mongodb://localhost/clients', { useNewUrlParser: true })
+
+const clientsSchema = new mongoose.Schema({
+    name: String,
+    last_name: String,
+    age: Number,
+    emails: Array,
+    business: String,
+    type: String,
+    orders: Array,
+});
+
+export const Clients = mongoose.model('clients', clientsSchema)
--- data/resolvers.js
+++ data/resolvers.js
@@ -1,25 +1,10 @@
-class Client {
-    constructor(id, {
-        name,
-        last_name,
-        age,
-        emails,
-        business,
-        type,
-        orders
-    }) {
-        this.id = id;
-        this.name = name;
-        this.last_name = last_name;
-        this.age = age;
-        this.emails = emails;
-        this.business = business;
-        this.type = type;
-        this.orders = orders;
-    };
-};
+// LIBS
+import mongoose from 'mongoose';
 
-const clientsDB = {};
+// DATABASE
+import { Clients } from './db';
+
+// ────────────────────────────────────────────────────────────────────────────────────────────────
 
 const resolvers = {
     Query: {
@@ -28,10 +13,24 @@ const resolvers = {
         },
     },
     Mutation: {
-        newClient: ({ form }) => {
-            const id = require('crypto').randomBytes(10).toString('hex');
-            clientsDB[id] = form;
-            return new Client(id, form);
+        createClient: (root, { form }) => {
+            const newClient = new Clients({
+                id: form.id,
+                name: form.name,
+                last_name: form.last_name,
+                age: form.age,
+                emails: form.emails,
+                business: form.business,
+                type: form.type,
+                orders: form.orders
+            });
+            newClient.id = newClient._id;
+            return new Promise((resolve, obj) => {
+                newClient.save((error) => {
+                    if (error) rejects(error);
+                    else resolve(newClient);
+                });
+            });
         },
     },
 };
